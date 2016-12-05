81c696919197b2832247e5a556cfe99a7f7b04d4
Andrew Arkebauer <andrewarkebauer@Andrews-MBP.wireless.yale.internal>
2016-12-04 20:52:16 -0500

    added contObs.m to yield whether system (A,B,C) is observable and/or controllable

---
+++ contObs.m
@@ -0,0 +1,37 @@
+function contObs(A,B,C)
+
+%% Controllability
+[~, dim] = size(A);
+
+for i=1:dim
+    if i == 1
+        contMat = B;
+    else
+        contMat = [contMat A^(i-1)*B];
+    end
+end
+
+if rank(contMat) == dim % if contMat is full rank
+    disp('System is Controllable')
+else
+    disp(['System is not Controllable. Rank = ' num2str(rank(contMat)) ' (must be ' num2str(dim) ')'])
+end
+
+
+%% Observability
+for i=1:dim
+    if i == 1
+        obsMat = C;
+    else
+        obsMat = [obsMat; C*A^(i-1)];
+    end
+end
+
+if rank(obsMat) == dim % if contMat is full rank
+    disp('System is Observable')
+else
+    disp(['System is not Observable. Rank = ' num2str(rank(obsMat)) ' (must be ' num2str(dim) ')'])
+end
+
+
+end
